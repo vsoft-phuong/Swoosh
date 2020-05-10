c5945721dbd45b306337b2ee516f4e64a4ab1894
Mr Phuong <vsoftphuong@gmail.com>
2020-05-10 23:43:52 +0700

    League activity click 3 button

---
+++ app/src/main/java/com/chatcore/swoosh/Constants.kt
@@ -0,0 +1,3 @@
+package com.chatcore.swoosh
+
+const val EXTRA_LEAGUE = "league"
--- app/src/main/java/com/chatcore/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/chatcore/swoosh/LeagueActivity.kt
@@ -2,7 +2,10 @@ package com.chatcore.swoosh
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
 
@@ -13,8 +16,37 @@ class LeagueActivity : BaseActivity() {
 
     }
 
-    fun leagueOnClicked(view: View){
-        val skillActivity = Intent(this, SkillActivity::class.java)
-        startActivity(skillActivity)
+    fun leagueOnClicked(view: View) {
+        if (selected != "") {
+            val skillActivity = Intent(this, SkillActivity::class.java)
+            skillActivity.putExtra(EXTRA_LEAGUE, selected)
+            startActivity(skillActivity)
+        }else{
+            Toast.makeText(this, "Please select a league", Toast.LENGTH_SHORT).show()
+        }
     }
+
+    var selected = ""
+    fun menBtnClicked(view: View) {
+        womenBtn.isChecked = false
+        coedBtn.isChecked = false
+
+        selected = if (menBtn.isChecked) menBtn.text.toString() else ""
+    }
+
+    fun womenBtnClicked(view: View) {
+        menBtn.isChecked = false
+        coedBtn.isChecked = false
+
+        selected = if (womenBtn.isChecked) womenBtn.text.toString() else ""
+    }
+
+    fun coedBtnClicked(view: View) {
+        womenBtn.isChecked = false
+        menBtn.isChecked = false
+
+        selected = if (coedBtn.isChecked) coedBtn.text.toString() else ""
+    }
+
+
 }
